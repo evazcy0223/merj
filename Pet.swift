dbf7d019f5bff4698cbfae11d8821ac785bdddc7
raustin520 <56164033+raustin520@users.noreply.github.com>
2019-12-04 17:14:46 -0500

    Update Pet.swift
    Adding states, correcting update function, removing all print statements (excepts those for testing purposes only)

--- Pet.swift
+++ Pet.swift
@@ -1,16 +1,22 @@
 import UIKit
 
-/*
+
 enum PetStates {
     case hungry
     case sleepy
     case healthy
 }
-*/
+
 
 class Pet {
+    enum State {
+        case hungry
+        case sleepy
+        case healthy
+    }
+    
+    var state : State
     var name : String
-    var healthy : Bool
     var sleepTime : Int
     var foodEaten : Int
     var kind : Character
@@ -19,11 +25,10 @@ class Pet {
     init(name: String, kind: Character) {
         self.name = name
         self.kind = kind
-        self.healthy = true
         self.sleepTime = 0
         self.foodEaten = 0
         self.color = "B"
-        print("Pet created!\n")
+        self.state = .healthy
     }
     
     func feedPet(numFood: Int) {
@@ -36,39 +41,59 @@ class Pet {
     
     func Update(time: Int) -> Bool {
         if time > self.sleepTime && time <= self.foodEaten {
-            print("I am sleepy\n")
-            // change state to sleepy
-            return true
+            if self.state == .sleepy {
+                return false
+            }
+            else {
+                self.state = .sleepy
+                return true
+            }
         }
         else if time > self.foodEaten && time <= self.sleepTime {
-            print("I am hungry\n")
-            // change state to hungry
-            return true
+            if self.state == .hungry {
+                return false
+            }
+            else {
+                self.state = .hungry
+                return true
+            }
         }
         else if time > self.sleepTime && time > self.foodEaten {
-            print("I am unhealthy\n")
-            // change state to unhealthy
-            return true
+            if self.state == .unhealthy {
+                return false
+            }
+            else {
+                self.state = .unhealthy
+                return true
+            }
         }
         else {
-            print("I am healthy\n")
-            // change state to healthy
-            return false
+            if self.state == .healthy {
+                return false
+            }
+            else {
+                self.state = .healthy
+                return true
+            }
         }
     }
+    
 }
 
 // making a checkpoint for Pet class
-
 let pet1 = Pet(name: "King", kind: "d")
 
 pet1.feedPet(numFood: 5)
-print("I have eaten", pet1.foodEaten, "things\n")
+
 
 pet1.goToSleep(numSleep: 8)
-print("I have slept", pet1.sleepTime, "hours\n")
+
 
 var somethingChanged : Bool
 somethingChanged = pet1.Update(time: 2)
+print(pet1.state, somethingChanged)
 somethingChanged = pet1.Update(time: 6)
+print(pet1.state, somethingChanged)
 somethingChanged = pet1.Update(time: 10)
+print(pet1.state, somethingChanged)
+
